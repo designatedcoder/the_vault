22d72eb6153333a613ec47b0582931ead5e2cbc3
designatedcoder <niko@designatedcoder.com>
2021-06-28 05:04:55 -0400

    Disabled notifications

--- webpack.mix.js
+++ webpack.mix.js
@@ -16,6 +16,7 @@ mix.js('resources/js/app.js', 'public/js').vue()
         require('postcss-import'),
         require('tailwindcss'),
     ])
+    .disableNotifications()
     .webpackConfig(require('./webpack.config'));
 
 if (mix.inProduction()) {
